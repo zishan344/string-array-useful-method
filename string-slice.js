82da12f9aebc6ac95895845e5142bf5bc5856f2c
zishan344 <zishanahmed344@gmail.com>
2022-02-14 11:40:55 +0600

    sone string slice

---
+++ string-slice.js
@@ -0,0 +1,25 @@
+const anthem = "Amar Sonar Bangla Ami Tomai Valobasi";
+const word = anthem.split(" ");
+// console.log(word);
+
+//slice
+const smallslicing = anthem.slice(0, 4);
+// console.log(smallslicing);
+
+// sub str
+
+//proble index number dea astesena kn
+const anotherPart = anthem.substr(0, 3);
+// console.log(anotherPart);
+
+// sub String
+const anotherPartParts = anthem.substring(0, 3);
+// console.log(anotherPartParts);
+const first = "amader";
+const second = "City";
+const fullString = first.concat(second);
+// console.log(fullString);
+
+const words = ["a", "b", "c", "d", "e"];
+const allJoined = words.join("");
+console.log(allJoined);
