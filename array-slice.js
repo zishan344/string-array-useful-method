7e5ad676bb29a405da312498a3632c222d2850ac
zishan344 <zishanahmed344@gmail.com>
2022-02-14 13:15:33 +0600

    doen the array slicing

---
+++ array-slice.js
@@ -0,0 +1,12 @@
+//slice
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+// const arrSlice = number.slice(0, 5);
+// console.log(arrSlice);
+
+//splice
+
+// const spliceNumber = number.splice(4, 3);
+// console.log(number);
+
+const splice2 = number.splice(4, 2, 155);
+console.log(number);
