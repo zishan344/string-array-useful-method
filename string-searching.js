eca3727691980624ae682465feaa12619019dd8c
zishan344 <zishanahmed344@gmail.com>
2022-02-14 10:17:00 +0600

    done string searching file

---
+++ string-searching.js
@@ -0,0 +1,49 @@
+const products = [
+  " hardcore i29 200GB laptop",
+  "iphone 1TB camera flashlight Phone",
+  "yellow laptop with black camera",
+  "1X59  laptop commercial yoga laptop Dell",
+  "LG supernova laptop",
+  "Htc low price Phone",
+  "purple color phone with Laptop",
+];
+
+//index of
+const searching = "Dell";
+const output = [];
+for (const product of products) {
+  //   console.log(product);
+  if (
+    product.toLocaleLowerCase().indexOf(searching.toLocaleLowerCase()) != -1
+  ) {
+    output.push(product);
+  }
+}
+//console.log(output);
+
+// includes dea searching
+
+// product.toLocaleLowerCase().includes(searching.toLocaleLowerCase()) != -1
+// // problem != not equal dile kaj korena kn
+for (const product of products) {
+  if (product.toLocaleLowerCase().includes(searching.toLocaleLowerCase())) {
+    output.push(product);
+  }
+}
+// console.log(output);
+
+//start with
+
+// problem 3 ta kore asar karon ki?
+for (const product of products) {
+  if (product.toLocaleLowerCase().startsWith(searching.toLocaleLowerCase())) {
+    output.push(product);
+  }
+}
+// console.log(output);
+for (const product of products) {
+  if (product.toLocaleLowerCase().endsWith(searching.toLocaleLowerCase())) {
+    output.push(product);
+  }
+}
+// console.log(output);
